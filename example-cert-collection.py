cac02859f48c1f53f48718707a66f77ce8675ba9
addy <109079846+anadditivity@users.noreply.github.com>
2025-11-19 12:38:18 +0200

    almost deleted all ee domains oops

--- example-cert-collection.py
+++ example-cert-collection.py
@@ -12,7 +12,20 @@ def get_cert(domain):
     cert = conn.getpeercert(True)
     return cert
 
-domains = ['example.com', 'neti.ee', 'facebook.com']
+domains = ['example.com', 'neti.ee',
+           # my domain that doesn't have a server nor an SSL cert
+           'anadditivity.eu',
+           # website that instantly redirects to another site
+           'sugu.ee',
+           'facebook.com']
+counter = 0
 for domain in domains:
-    with open(f'certificates/{domain}.pem', 'wb') as f:
-        f.write(get_cert(domain))
+    try:
+        cert = get_cert(domain)
+        with open(f'certificates/{domain}.pem', 'wb') as f:
+            f.write(cert)
+        counter += 1
+    except ssl.SSLError:
+        continue
+
+print(f'Out of {len(domains)}, managed to get {counter} certificates')
